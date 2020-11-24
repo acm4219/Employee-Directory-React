29ea1afb1b63d6248d154595971dfa399478306d
Alex McDonald <acm4219@gmail.com>
2020-11-23 18:18:28 -0600

    scrapping most of the former project starting over again

--- employeedirectory/src/components/EmployeeTable.js
+++ employeedirectory/src/components/EmployeeTable.js
@@ -3,7 +3,30 @@ import React from 'react';
 function EmployeeTable(props) {
     return (
         <div>
-
+        <div className="text-center">
+        <table>
+            <thead>
+            <tr>
+             <th>Id Picture</th>
+            <th>Name: </th>
+             <th>Id: </th>
+             <th>Phone: </th>
+            <th>Email: </th>
+            <th>Date of Birth: </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+            <td><img alt={props.name} className="img-fluid" src={props.picture} style={{ margin: "0 auto"}} /></td>
+            <td>{props.name}</td>
+            <td>{props.id}</td>
+            <td>{props.phone}</td>
+            <td>{props.email}</td>
+            <td>{props.dob}</td> 
+            </tr> 
+            </tbody>
+            </table>     
+            </div>
         </div>
     )
 }
